6a70b12d87040648b52eb5a6d29da75f9425221d
arzylmanzano <arzyl.manzano001@gmail.com>
2021-11-29 15:59:45 +0800

    - added hanging knee connection

--- WindowsFormsApp2/Exercises/Types/Male/Abdominal/frmManAbdominalEx.cs
+++ WindowsFormsApp2/Exercises/Types/Male/Abdominal/frmManAbdominalEx.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp2.Exercises.Types.Male.Abdominal;
 
 namespace WindowsFormsApp2.Exercises.Types.Male.Abdominal
 {
@@ -33,6 +34,12 @@ namespace WindowsFormsApp2.Exercises.Types.Male.Abdominal
         private void pictureBox3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmHangingKnee hangingKnee = new frmHangingKnee();
+            hangingKnee.Show();
         }
     }
 }
